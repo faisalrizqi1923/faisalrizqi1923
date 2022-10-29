d812f57a936f7226e6d861b29e415206be6e7147
holmes_ <88739612+faisalrizqi1923@users.noreply.github.com>
2022-10-29 13:42:42 +0700

    Add files via upload
    simple calculator with function

---
+++ proyek 2.py
@@ -0,0 +1,51 @@
+# from curses import def_shell_mode
+
+
+# class angka:
+#     jumlah = 10
+
+# print(a.jumlah)
+
+
+
+
+def faisal(angka1,angka2):
+
+    hasil = angka1 + angka2
+    print(f"{angka1} + {angka2} = {hasil}")
+
+faisal(23,1)
+
+
+
+def fungsi(angka_1,angka_2):
+    tambah=  angka_1 +angka_2
+    kurang=  angka_1 - angka_2
+    kali= angka_1 * angka_2
+    bagi = angka_1 / angka_2
+
+    return tambah,kurang,kali,bagi
+
+
+tambah,kurang,kali,bagi = fungsi(9,3)
+    
+print(f"hasil  tambah = {tambah}")
+print(f"hasil kurang = {kurang}")
+print(f"hasil  kali = {kali}")
+print(f"hasil bagi = {bagi}")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
